8f5583015a8cf8d7877a6e1236ad35afacc7f260
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2024-09-08 12:57:38 -0300

    se declaran los atributos de la clase user

---
+++ app/src/main/java/com/marceloacuna/semana_5/Model/User.kt
@@ -0,0 +1,10 @@
+package com.marceloacuna.semana_5.Model
+
+data class User (
+    val nombreCompleto: String,
+    val email:String,
+    val direccion: String,
+    val password:String,
+    val fechaNac: String
+)
+{}
